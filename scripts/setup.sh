2f5c1d685eab5e40d4cb8ea03aec6f534b7d8668
James Lee-Jones <james@lee-jones.co.uk>
2024-11-28 17:22:41 +0000

    feat: Add github action to run all single_file tests

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,6 @@ set -x
 test -d ./AlloyModel
 test -d ./test
 test -d ./scripts
-test -d ./third_party
 
+python3 -m pip install -r requirements.txt
 wget -P ./third_party https://github.com/AlloyTools/org.alloytools.alloy/releases/download/v6.0.0/org.alloytools.alloy.dist.jar
